236690a1cb781e1c32ba83c7a93680048e67505e
Nirajchemjong <nirajlimbu7@gmail.com>
2023-10-27 13:44:15 +1100

    add Task router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const taskRouter = require("./src/router/taskRouter");
 const app = express();
 
 //Basic middleware
@@ -8,6 +9,15 @@ app.use(express.json());
 app.get("/healthy", (req, res) => {
   res.json({ status: "Healthy" });
 });
+
+/*
+
+Building ApI 
+
+
+*/
+
+app.use("/api/v1/task", taskRouter);
 
 const PORT = process.env.PORT || 2005;
 app.listen(PORT, () => {
